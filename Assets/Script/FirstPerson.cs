7544d2fefad67a14e69c7389177164dde7917cf8
AR3ZzZz <163415664+AR3ZzZz@users.noreply.github.com>
2024-10-17 19:27:02 +0200

    FPSdia1

--- Assets/Script/FirstPerson.cs
+++ Assets/Script/FirstPerson.cs
@@ -25,6 +25,17 @@ public class FirstPerson : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         Vector3 movimiento = new Vector3 (h, 0, v).normalized;
-        characterController.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+
+        float anguloRotacion =Mathf.Atan2(movimiento.x, movimiento.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+
+        if (movimiento.magnitude > 0)
+        {
+         //Orientacion hacia el movimiento
+          transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+         //Me muevo hacia donde miro
+         characterController.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+
+        }
     }
 }
