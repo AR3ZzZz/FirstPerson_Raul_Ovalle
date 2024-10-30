268644aab46f9650f649cd58a604ffb6e78056be
AR3ZzZz <163415664+AR3ZzZz@users.noreply.github.com>
2024-10-30 17:09:04 +0100

    NavMesh

--- Assets/Script/FirstPerson.cs
+++ Assets/Script/FirstPerson.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class FirstPerson : MonoBehaviour
     [Header("Mov")]
     [SerializeField] float velocidadMovimiento;
     [SerializeField] float factorGravedad;
+    [SerializeField] float alturaSalto;
 
     [Header("DeteccionSuelo")]
     [SerializeField] float radioDeteccion;
@@ -18,12 +20,30 @@ public class FirstPerson : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;   //Centra en la raton y lo oculta
     }
 
     void Update()
     {
         MoverYRotar();
+        AplicarGravedad();
+
+        if (EnSuelo())
+        {
+            movVertical.y = 0;
+            Salto();
+        }
+        
     }
+
+    private void Salto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movVertical.y += Mathf.Sqrt(-2 * factorGravedad * alturaSalto);
+        }
+    }
+
     void MoverYRotar()
     {
         float h = Input.GetAxisRaw("Horizontal");
@@ -38,24 +58,20 @@ public class FirstPerson : MonoBehaviour
         {
              Vector3 movimiento = Quaternion.Euler (0, angulo, 0) * Vector3.forward;
     
-            transform.eulerAngles = new Vector3(0, angulo, 0);
+             transform.eulerAngles = new Vector3(0, angulo, 0);
             
              //Me muevo hacia donde miro
              characterController.Move(movimiento * velocidadMovimiento * Time.deltaTime);
         }
 
-        EnSuelo();
-        if (EnSuelo())
-        {
-             AplicarGravedad();
-        }
+        
     }
 
     void AplicarGravedad()
     {
-        movVertical.y += factorGravedad * Time.deltaTime * Time.deltaTime;
+        movVertical.y += factorGravedad * Time.deltaTime;
         //Gravedad M/S2
-        characterController.Move(movVertical);
+        characterController.Move(movVertical * Time.deltaTime);
     }
 
     bool EnSuelo()
@@ -65,5 +81,9 @@ public class FirstPerson : MonoBehaviour
         return result;
     }
 
-
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(pies.position, radioDeteccion);
+    }
 }
