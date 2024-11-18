a78dc07ed3d05ae49b47b1eb578839828dffe90c
AR3ZzZz <163415664+AR3ZzZz@users.noreply.github.com>
2024-11-18 20:50:43 +0100

    WeaponSwap

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -6,6 +6,12 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] Transform[] Spawns;
+    [SerializeField] int spawnsPerLevel;
+    [SerializeField] int roundsPerLevel;
+    [SerializeField] int levelNumber;
+    [SerializeField] float timeBetweenSpawn;
+    [SerializeField] float timeBetweenRound;
+    [SerializeField] float timeBetweenLevels;
 
     void Start()
     {
@@ -19,11 +25,24 @@ public class Spawner : MonoBehaviour
 
     IEnumerator SpawnEnemy()
     {
-        while (true) 
+        for (int actualLevel = 0; actualLevel < levelNumber; actualLevel++)
         {
-            Instantiate(enemyPrefab, Spawns[Random.Range(0, Spawns.Length)].position, Quaternion.identity);
-            yield return new WaitForSeconds(10);
-        }
+            for (int actualRound = 0; actualRound < roundsPerLevel; actualRound++)
+            {
+                for (int spawnNumber = 0; spawnNumber < spawnsPerLevel; spawnNumber++)
+                {
+                    Instantiate(enemyPrefab, Spawns[Random.Range(0, Spawns.Length)].position, Quaternion.identity);
+                    yield return new WaitForSeconds(timeBetweenSpawn);
+                    
+                }
+
+                //text new level
+                yield return new WaitForSeconds(timeBetweenRound);
+            }
+
+            //text new level
+            yield return new WaitForSeconds(timeBetweenLevels);
+        }        
        
     }
 }
---
+++ Assets/Script/SwapWeapon.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject[] weapons;
+    int actualWeapon;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeaponSwapKeys();
+        WeaponSwapMouse();
+    }
+
+    private void WeaponSwapMouse()
+    {
+        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollWheel > 0)
+        {
+            WeaponSwap(actualWeapon + 1);
+        }
+        else
+        {
+            WeaponSwap(actualWeapon - 1);
+        }
+    }
+
+    private void WeaponSwapKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            WeaponSwap(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            WeaponSwap(1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            WeaponSwap(2);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            WeaponSwap(3);
+
+        }
+    }
+
+    private void WeaponSwap(int newWeaponNumber)
+    {
+        weapons[actualWeapon].SetActive(false);
+
+        if (newWeaponNumber < 0)
+        {
+            newWeaponNumber = weapons.Length - 1;
+        }
+        else if (newWeaponNumber > weapons.Length)
+        {
+            newWeaponNumber = 0;
+        }
+
+        weapons[newWeaponNumber].SetActive(true);
+
+        actualWeapon = newWeaponNumber;
+    }
+}
